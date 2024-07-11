7d03970407407c4107dbc3e2dfc73ea18a24ac75
arnab-spirinova <alfatitas747ray@outlook.com>
2024-07-11 15:23:25 +0530

    feat: updated Permissions

--- contracts/CreateProtocolPermissions.sol
+++ contracts/CreateProtocolPermissions.sol
@@ -4,11 +4,25 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 contract CreateProtocolPermissions is OwnableUpgradeable{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    mapping(address=>bool) public minterList;
+    mapping( address => bool ) public minterList;
+    mapping( address => bool ) public creatorList;
 
     modifier onlyMinter()  {
         require(minterList[msg.sender],"User not a minter");
         _;
+    }
+
+    modifier onlyCreator() {
+        require(creatorList[msg.sender],"User not a creator");
+        _;
+    }
+
+    function grantMinterRole(address _minter) public onlyOwner {
+        minterList[_minter] = true;
+    }
+
+    function grantCreatorRole(address _creator) public onlyOwner {
+        creatorList[_creator] = true;
     }
 
 }
