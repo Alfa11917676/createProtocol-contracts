a4c9bf56cd83b2b9fa38b6bb8e2f39c09f4b4753
arnab-spirinova <alfatitas747ray@outlook.com>
2024-07-11 15:24:25 +0530

    feat: added interfaces

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,6 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-
-
   networks: {
     goerli: {
       url: "https://goerli.infura.io/v3/3932027bc24b4df089d1ab33886ad3db",
