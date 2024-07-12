6d63b84ffa356468dc9b82892920387e20eea807
garima <garimamangalclg@gmail.com>
2024-07-12 13:21:59 +0530

    adds: deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,29 @@
 import { ethers } from "hardhat";
+import hre from "hardhat";
 
 async function main() {
+  const createProtocolRegistry = await hre.ethers.getContractFactory(
+    "CreateProtocolRegistry"
+  );
+  const CreateProtocolRegistry = await createProtocolRegistry.deploy();
 
+  const createProtocol = await hre.ethers.getContractFactory("CreateProtocol");
+  const CreateProtocol = await createProtocol.deploy();
+
+  const createProtocolPermissions = await hre.ethers.getContractFactory(
+    "CreateProtocolPermissions"
+  );
+  const CreateProtocolPermissions = await createProtocolPermissions.deploy();
+
+  const deployedAddress = {
+    CreateProtocolRegistry: CreateProtocolRegistry.target,
+    CreateProtocol: CreateProtocol.target,
+    CreateProtocolPermissions: CreateProtocolPermissions.target,
+  };
+  console.log(deployedAddress);
 }
 
-async function verify(contractAddress:string, contructorArgs:any[]) {
+async function verify(contractAddress: string, contructorArgs: any[]) {
   await hre.run("verify:verify", {
     address: contractAddress,
     constructorArguments: [contructorArgs],
